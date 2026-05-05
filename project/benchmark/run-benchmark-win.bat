be8a40cc41506d71d9dff1619bd966b5995a3ed0
Marcelo <marcelocicogna@gmail.com>
2026-05-05 16:00:38 -0300

    Adds Code::Blocks 25.03 support (gcc MingW-W64 14.2.0).

---
+++ project/benchmark/run-benchmark-win.bat
@@ -0,0 +1,8 @@
+..\build\debug\gwo.exe -name F1 -population_size 100 -iterations 1000
+..\build\debug\gwo.exe -name F2 -population_size 100 -iterations 1000
+..\build\debug\gwo.exe -name F3 -population_size 100 -iterations 1000
+..\build\debug\gwo.exe -name F4 -population_size 100 -iterations 1000
+..\build\debug\gwo.exe -name F5 -population_size 100 -iterations 1000
+..\build\debug\gwo.exe -name F6 -population_size 100 -iterations 1000
+
+pause
